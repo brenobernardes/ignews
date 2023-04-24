0edb67718efa97c7fbddac889e5d9bef73ddf5e1
brenobernardes <breno.pbernardes@gmail.com>
2023-04-24 15:42:58 -0300

    connecting to prismic api

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,12 +1,12 @@
-import Prismic from '@prismicio/client';
+import Prismic from '@prismicio/client'
 
 export function getPrismicClient(req?: unknown) {
-    const prismic = Prismic.createClient(
-        process.env.PRISMIC_ACCESS_TOKEN,
-        {
-            //req: req,
+    const prismic = Prismic.client(
+        process.env.PRISMIC_ENDPOINT,
+        { 
+            req,
             accessToken: process.env.PRISMIC_ACCESS_TOKEN
-        }
+        },
     )
 
     return prismic;
