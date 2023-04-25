1660b1d7cad1a94cb5898dbef8248763ae627b8d
brenobernardes <breno.pbernardes@gmail.com>
2023-04-25 17:40:49 -0300

    redirecting subscribe button

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -2,6 +2,7 @@ import { signIn, useSession } from 'next-auth/react';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
+import { useRouter } from 'next/router';
 
 interface SubscribeButtonProps {
     priceId: string
@@ -9,10 +10,16 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton ({priceId}: SubscribeButtonProps) {
     const session = useSession();
+    const router = useRouter();
 
     async function handleSubscribe() {
         if (!session) {
             signIn('github');
+            return;
+        }
+
+        if(session.activeSubscription) {
+            router.push('/post');
             return;
         }
 
